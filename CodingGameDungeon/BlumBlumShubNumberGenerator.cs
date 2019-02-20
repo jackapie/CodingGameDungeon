f01d54513dffb477d00971411064dd60a87043cf
Jackie Pearce <jackiepearce83@hotmail.com>
2019-02-20 17:46:25 +0000

    Wrote Blum Blum Shub random number generator

---
+++ CodingGameDungeon/BlumBlumShubNumberGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGameDungeon
+{
+    public class BlumBlumShubNumberGenerator
+    {
+       
+
+        int LeastCommonMultiple(int a, int b)
+        {
+            int num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (int i = 1; i < num2; i++)
+            {
+                if ((num1 * i) % num2 == 0)
+                {
+                    return i * num1;
+                }
+            }
+            return num1 * num2;
+        }
+                
+        public int LambdaM(int p, int q)
+        {
+            return LeastCommonMultiple(p - 1, q - 1);
+        }
+
+        public int Power(int p, int q, int n)
+        {
+
+            var factor = new BigInteger(1);
+            for (int i = 0; i < n; i++)
+            {
+                factor = factor * 2;
+            }
+            return (int)(factor % LambdaM(p, q));
+        }
+
+        public int GenerateNthTerm(int r, int p, int q, int n)
+        {
+            var power = Power(p, q, n);
+            var factor = new BigInteger(1);
+            for (int i = 0; i < power; i++)
+            {
+                factor = factor * r;
+            }
+
+            return (int)(factor % (p * q));
+        }
+
+        public int GenerateNextTerm(int xn, int p, int q)
+        {
+            return (xn * xn) % (p * q);
+        }
+    }
+}
---
+++ CodingGameDungeonTests/BlumBlumShubNumberGeneratorTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingGameDungeon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGameDungeon.Tests
+{
+    [TestClass()]
+    public class BlumBlumShubNumberGeneratorTests
+    {
+        [TestMethod()]
+        public void GenerateNextTermTest()
+        {
+            var numberGenerator = new BlumBlumShubNumberGenerator();
+            var zerothTerm = numberGenerator.GenerateNextTerm(3, 11, 19);
+            var firstTerm = numberGenerator.GenerateNextTerm(zerothTerm, 11, 19);
+            var secondTerm = numberGenerator.GenerateNextTerm(firstTerm, 11, 19);
+            var thirdTerm = numberGenerator.GenerateNextTerm(secondTerm, 11, 19);
+            var fourthTerm = numberGenerator.GenerateNextTerm(thirdTerm, 11, 19);
+            var fifthTerm = numberGenerator.GenerateNextTerm(fourthTerm, 11, 19);
+            Assert.AreEqual(9, zerothTerm);
+            Assert.AreEqual(81, firstTerm);
+            Assert.AreEqual(82, secondTerm);
+            Assert.AreEqual(36, thirdTerm);
+            Assert.AreEqual(42, fourthTerm);
+            Assert.AreEqual(92, fifthTerm);
+
+        }
+
+        [TestMethod()]
+        public void GenerateNthTermTest()
+        {
+            var numberGenerator = new BlumBlumShubNumberGenerator();
+            //var fourthTerm = numberGenerator.GenerateNthTerm(3, 11, 19, 5);
+            var fifthTerm = numberGenerator.GenerateNthTerm(3, 11, 19, 6);
+            //Assert.AreEqual(42, fourthTerm);
+            Assert.AreEqual(92, fifthTerm);
+        }
+
+        [TestMethod()]
+        public void LambdaMTest()
+        {
+            var numberGenerator = new BlumBlumShubNumberGenerator();
+            var result = numberGenerator.LambdaM(11, 19);
+            Assert.AreEqual(90, result);
+        }
+
+        [TestMethod()]
+        public void PowerTest()
+        {
+            var numberGenerator = new BlumBlumShubNumberGenerator();
+            var result = numberGenerator.Power(11, 19, 4);
+            Assert.AreEqual(16, result);
+        }
+    }
+}
